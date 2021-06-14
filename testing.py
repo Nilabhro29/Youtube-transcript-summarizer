eebbe42cd61980fb37297eefb067730602c58fd1
abhayjadavpur <70446888+abhayjadavpur@users.noreply.github.com>
2021-06-14 13:37:39 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,7 +5,7 @@ from summarizer import Summary
 
 
 def execute(ytube_id, percent):
-    print('********get transcript from url*******\n')
+    print('********nilabhro bhen ki chut gandu saala ma la laoura madarchod get transcript from url*******\n')
     transcript = get_transcript_from_url(ytube_id)
     print(transcript)
     print('**********raw transcript***********\n')
